8d2877d3a6111c7babb22202061b86a22b861798
Victor Zuluaga <zrvictor@outlook.com>
2020-12-07 17:24:57 -0500

    execute and kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # killing a process
 exec { 'kill':
-  path     => ['/home/victorz/Holberton/holberton-system_engineering-devops/0x0A-configuration_management'],
   command  => 'pkill -f killmenow',
   provider => 'shell',
 }

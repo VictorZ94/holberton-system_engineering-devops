44949c62905aa7ad1e2d9f3ce045842b0d637dd0
Victor Zuluaga <zrvictor@outlook.com>
2020-12-07 16:16:46 -0500

    create a file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# My first puppet file
+file { 'holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}

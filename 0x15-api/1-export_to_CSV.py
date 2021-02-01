fb1056697a651f447fa3c04ab367eda48a514c5b
Victor Zuluaga <zrvictor@outlook.com>
2021-02-01 16:27:41 -0500

    export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,14 +9,13 @@ import sys
 if __name__ == "__main__":
     """working on an API
     """
-    number_of_task_done = 0
-    total_number_of_task = 0
-    # get user and extrac his/her name
+    # get user and extrac his/her username
     response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                             .format(sys.argv[1]))
-    get_user = response.json().values()
-    username = list(get_user)[2]
+    get_user = response.json()
+    username = get_user.get('username')
 
+    # get Completed and title
     new_reponse = requests.get('https://jsonplaceholder.typicode.com/todos/\
 ?userId={}'.format(sys.argv[1]))
     get_task = new_reponse.json()
@@ -24,6 +23,6 @@ if __name__ == "__main__":
     with open('{}.csv'.format(sys.argv[1]), mode="w") as File_csv:
         Api_writer = csv.writer(File_csv, quotechar='"', quoting=csv.QUOTE_ALL)
         for item in get_task:
-            Api_writer.writerow((sys.argv[1], username,
-                                 item.get('completed'),
-                                 item.get('title')))
+            Fields_writer = [sys.argv[1], username, item.get('completed'),
+                             item.get('title')]
+            Api_writer.writerow(Fields_writer)

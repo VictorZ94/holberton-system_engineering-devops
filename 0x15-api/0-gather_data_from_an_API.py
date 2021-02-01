5db0935ca34596ad5c8c450a6fc2854b4210ef55
Victor Zuluaga <zrvictor@outlook.com>
2021-02-01 15:16:57 -0500

    gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Using and API and work in Json
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """working on an API
+    """
+    number_of_task_done = 0
+    total_number_of_task = 0
+    # get user and extrac his/her name
+    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(sys.argv[1]))
+    get_user = response.json().values()
+    user = list(get_user)[1]
+    # get task
+    new_reponse = requests.get('https://jsonplaceholder.typicode.com/todos/\
+                               ?userId={}'.format(sys.argv[1]))
+    get_task = new_reponse.json()
+    # for to count number of task
+    for item in get_task:
+        if item.get('completed') is True:
+            number_of_task_done += 1
+        total_number_of_task += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user, number_of_task_done, total_number_of_task))
+    for item in get_task:
+        if item.get('completed') is True:
+            print("\t{}".format(item.get('title')))
+
